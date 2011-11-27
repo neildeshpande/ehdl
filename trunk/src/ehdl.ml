88f27ed4d02ec42ac9b8c4cfe1d81a638871e24b
mashooqm <mashooqm@a9ed8438-15c7-ac90-eaa9-0487342f3641>
2011-11-27 17:57:56 +0000

    Made Call a statement, handling Call, prelim versions Aasn, subbus and barraym unique sens list, multiple functions

--- trunk/src/ehdl.ml
+++ trunk/src/ehdl.ml
@@ -7,11 +7,52 @@ module CompMap = Map.Make(struct
 end)
 
 
+type s_env = {
+    sens_list : string list
+           } (* can add more stuff to this, list of locals for example, so POS can update it *) 
+
+
+(* insert non-duplicates *) 
+let rec insert_uniq (l:string list) name =
+	try let _ = List.find ( fun ( s ) -> s = name ) l in l  
+	with Not_found-> name::l
+
+(* returns a list whose vals are unique *) 	
+let uniq lst = 
+    List.fold_left (fun l s -> insert_uniq l s) [] lst    	
+
+
+(* Utility function for producing a delimeter separated string from a list*)    
+let rec delim_sprt delim p = match List.length p with 
+       0 -> "" | 
+       x -> let head = (List.hd p)
+	      (* don't want to put in a delim after the last element, hence the if check *) 	 
+	     in if ( x = 1 ) then head else  (head ^ delim ^ (delim_sprt delim (List.tl p)) )   
+
+let rec port_descr_list p inOrOut (ports: Ast.bus list) = match ports with
+       [] -> p
+       | hd::tl -> let typedescr = 
+		     (match hd.size with 
+			0 -> raise (Failure ("bus size cannot be zero " ))
+			(* not doing std_logic because then we have to convert 1 to '1' *)
+		  | x -> " std_logic_vector(" ^ string_of_int(hd.size-1) ^ " downto 0)" ) 
+		   in  let s = "\t" ^ hd.name ^ " : "  ^ inOrOut ^ typedescr 
+		       in port_descr_list  (s::p) inOrOut tl	     
+	     
+	     
+let port_gen cname cobj = 
+    let inportlist = port_descr_list [] "in " cobj.pin
+	in let portList =  port_descr_list inportlist "out" cobj.pout
+	  in delim_sprt ";\n" (List.rev portList)       
+	     
+	     
+	    
 (*Auxiliary function: adds conv_std_logic_vector *)
 let num_to_slv v size =
    try let _ = int_of_string v
 	in "conv_std_logic_vector("^v^","^(string_of_int size)^")"
-   with Failure(s) -> v
+   with Failure(s) -> v	     
+	     
 
 let translate (genv, ftable) =
 let create_component cname cobj components = 
@@ -20,43 +61,26 @@ let create_component cname cobj components =
     "use ieee.std_logic_arith.all;\n" ^
     "use ieee.std_logic_signed.all;\n\n\n"
 
-  (* Utility function for producing a delimeter separated string from a list*) 
-  in let rec delim_sprt delim p = match List.length p with 
-       0 -> "" | 
-       x -> let head = (List.hd p)
-	      (* don't want to put in a delim after the last element, hence the if check *) 	 
-	     in if ( x = 1 ) then head else  (head ^ delim ^ (delim_sprt delim (List.tl p)) ) 	   
-   
-   (* helper for building the vhdl style port list string *) 		 
-     in let rec port_descr_list p inOrOut (ports: Ast.bus list) = match ports with
-       [] -> p
-       | hd::tl -> let typedescr = 
-		     (match hd.size with 
-			0 -> raise (Failure ("bus size cannot be zero " ))
-			(* not doing std_logic because then we have to convert 1 to '1' *)
-		  | x -> " std_logic_vector(" ^ string_of_int(hd.size-1) ^ " downto 0)" ) 
-		   in  let s = "\t" ^ hd.name ^ " : "  ^ inOrOut ^ typedescr 
-		       in port_descr_list  (s::p) inOrOut tl
-   
-
    in let entity cname cobj = (* entity *) 
-	let inportlist = port_descr_list [] "in " cobj.pin
-	in let portList =  port_descr_list inportlist "out" cobj.pout
-	  in let s = delim_sprt ";\n" (List.rev portList)       
+	    let s = port_gen cname cobj     
 	     in "entity " ^ cname ^ "  is \n\nport (\n" ^
 		"\tclk : in std_logic;\n\trst : in std_logic;\n" ^ s ^ ");\n\nend main;\n\n"   
 	     
+   
 (* Evaluate expressions *) 
- in let rec eval e l = match e with
-    Num(i) -> string_of_int i, l 
-    | Id(i) -> i, i::l (* the list is keeping track of variables for the sensitivity list*) 
-    | Unop(op,e1) -> let v1, l = eval e1 l in 
+ in let rec eval e env = match e with
+    Num(i) -> string_of_int i, env 
+    | Id(i) -> i, {sens_list = i::env.sens_list;} (* the list is keeping track of variables for the sensitivity list*) 
+    | Barray(bs, idx, _) -> bs.name ^ "[" ^ (string_of_int idx) ^ "]" (* will need to consider if we let the programmar use expr at all. Right now it is using the size as index, which is inaccurate *), {sens_list = bs.name::env.sens_list;} (* right now using "a" rather than "a[i]" in the sensitivity list *)  
+	| Subbus(bs, strt, stop) -> bs.name ^ "(" ^ (string_of_int strt) ^ ":" ^ (string_of_int stop) ^ ")", 
+	{sens_list =  bs.name::env.sens_list; }      
+    | Unop(op,e1) -> let v1, env = eval e1 env in 
     ( match op with 
       Umin -> "- " ^ v1  
     | Not -> "not " ^ v1
-    | x -> raise (Failure ("Unary operator not supported ")) ), l 
+    | x -> raise (Failure ("ERROR: Invalid Unary Operator ")) ), env 
     | Binop(e1,op,e2) -> 
-     let v1, l = eval e1 l  in let v2, l = eval e2 l 
+     let v1, env = eval e1 env  in let v2, env = eval e2 env 
      in (match op with 
 	 Add  -> v1 ^ " + " ^ v2
        | Sub  -> v1 ^ " - " ^ v2 
@@ -74,62 +98,90 @@ let create_component cname cobj components =
        | Xor  -> v1 ^ " xor  " ^ v2
        | Shl  -> v1 ^ " sll " ^ v2 (*check that v2 is an integer,here or parser ? *)
        | Shr  -> v1 ^ " srl " ^ v2 
-       | x    -> raise (Failure ("The operator is not a binary operator "))), l
-   | Basn(i, e1) -> let v1, l = eval e1 l
+       | x    -> raise (Failure ("ERROR: Invalid Binary Operator "))), env
+   | Basn(i, e1) -> let v1, env = eval e1 env
 		in  let slv_v1 = num_to_slv v1 i.size
-		  in (i.name ^ " <= " ^ slv_v1 ^ ";\n" ) , l
-   | Call(i,e1) ->  raise (Failure ("Call not supported yet " ))
-   
+		  in ("\t\t" ^ i.name ^ " <= " ^ slv_v1 ^ ";\n" ) , env   
+   | Aasn(i,sz,e1,e2) -> let v1, env = eval e1 env (* may want to restrict what e1 can be, I say just make num or const *)
+             in let v2, env = eval e2 env 
+		     in  let slv_v2 = num_to_slv v2 i.size
+		     in ("\t\t" ^ i.name ^ "[" ^ v1 ^ "] " ^ " <= " ^ slv_v2 ^ ";\n" ) , env    		  
    | x ->  raise (Failure ("Expression not supported yet " ))
 
- (* statements *)
- in let rec translate_stmt (l,str) stmt = 
+ (* translate_Stmt *)
+ in let rec translate_stmt (env,str) stmt = 
     (  match stmt with  
-	  Block(stmts)  -> List.fold_left translate_stmt (l,str) (List.rev stmts) 
-	| Expr(e) -> let s,l = eval (fst e) l  in (l, (str ^ s))   
+	  Block(stmts)  -> List.fold_left translate_stmt (env,str) (List.rev stmts) 
+	| Expr(e) -> let s,env = eval (fst e) env  in (env, (str ^ s))   
 	| If(e,if_stmt,else_stmt) -> 
-	    let s,l = eval e l 
-	    in let l,if_block = translate_stmt(l,"") if_stmt
-	    in let l,else_block = translate_stmt(l,"") else_stmt
-	    in l, ("if (" ^ s ^ ") then \n\t\t\t" ^ if_block 
+	    let s,env = eval e env 
+	    in let env,if_block = translate_stmt(env,"") if_stmt
+	    in let env,else_block = translate_stmt(env,"") else_stmt
+	    in env, ("\t\tif (" ^ s ^ ") then \n" ^ if_block 
 	    (* the tabbing needs to be done programmatically, not manually. 
 	    I am assuming SAST will tell us the nesting depth *)    
-	    ^ "\n\t\telse\n\t\t\t" ^ else_block ^ "\n\t\tend if;") 
+	    ^ "\n\t\telse\n" ^ else_block ^ "\t\tend if;\n") 
 	| Switch ( e, c_list ) -> 
 	    ( match c_list with 
-	      [] -> l,""
+	      [] -> env,""
 	     |hd::tl ->       
-	     let s,l = eval e l 
+	     let s,env = eval e env 
            in let (e1, stmt) = hd
-           in let s1,l = eval e l in let s2,l = eval e1 l    
-           in let s3 = "if (" ^ s1 ^ " = " ^ s2 ^ ") then \n\t\t\t"  
-           in let l,if_block = translate_stmt (l,"") stmt
-		   in let l,s5 = List.fold_left (translate_case s1) (l,"") tl 	
-		   in l, (s3 ^ if_block ^ s5 ^ "\n\t\tend if;") ) 		      
-	| Pos(s2) -> raise (Failure ("Pos not supported yet " ))     
+           in let s1,env = eval e env in let s2,env = eval e1 env    
+           in let s3 = "\t\tif (" ^ s1 ^ " = " ^ s2 ^ ") then \n"  
+           in let env,if_block = translate_stmt (env,"") stmt
+		   in let env,s5 = List.fold_left (translate_case s1) (env,"") tl 	
+		   in env, (s3 ^ if_block ^ s5 ^ "\t\tend if;\n") ) 		      
+	| Pos(s2) -> raise (Failure ("Pos not supported yet " ))    
+	| Call(fdecl, out_list, in_list ) ->
+	   
+	    (* need to fix for multiple instances of the same component *) 
+	    let s = str ^ fdecl.fid ^ "_0 : " ^ fdecl.fid ^ " port map (\n\t\tclk => clk,\n \t\trst => rst" 
+	    in let f (s,env,l) b = 
+	    let s1,env = (match (List.hd l) with 
+	     Id(i) -> i, {sens_list = i::env.sens_list;} (* need to handle sensitivity list *) 
+	   | Barray(bs, idx, _) -> bs.name ^ "[" ^ (string_of_int idx) ^ "]" (* will need to consider if we let the programmar use expr at all *), {sens_list = bs.name::env.sens_list;} (* right now using a rather than a[i] in the sensitivity list *)  
+	   | Subbus(bs, strt, stop) -> bs.name ^ "(" ^ (string_of_int strt) ^ ":" ^ (string_of_int stop) ^ ")", {sens_list = bs.name::env.sens_list; }  
+	   | x ->  raise (Failure ("In/Output port mapping must use pre-existing variables " ))   ) in  s^",\n\t\t"^b.name^" => " ^ s1 , env, List.tl l    
+	    
+	    in let env = {sens_list = "rst"::("clk"::env.sens_list);}  
+	    in let s,env,_ = List.fold_left f (s,env,in_list) fdecl.pin
+	    in let s,_,_ = List.fold_left f (s,{sens_list=[];},out_list) fdecl.pout (* discard senstivity list updates due to the output ports *)  
+	    in env, s ^ ");\n"     
 	| x -> 	raise (Failure ("Statement not supported yet " )) )
-    and translate_case left (l,s) (e,stmt) = 
+    and translate_case left (env,s) (e,stmt) = 
       ( match e with 
      (* SAST needs to check there is atmost one dafault and no duplicate 
      case expressions *) 
-          Noexpr->   translate_stmt (l,s ^ "\t\telse \n\t\t\t") stmt   
-        | x     ->    let right,l = eval e l 
-         in translate_stmt (l,s ^ "\t\telsif (" ^ left ^ " = " ^ right ^ ") then \n\t\t\t" ) stmt  
+          Noexpr->   translate_stmt (env,s ^ "\t\telse \n") stmt   
+        | x     ->    let right,env = eval e env 
+         in translate_stmt (env,s ^ "\t\telsif (" ^ left ^ " = " ^ right ^ ") then \n" ) stmt  
          )
+(* end of translate_stmt *)          
+         
 
-    in let print_process prev (l,s) =  
-	 let ss = delim_sprt ", " (List.rev l)
-	 in prev ^ "\n\tprocess (" ^ ss ^ ")\n\tbegin\n\t\t" ^ s ^ "\n\tend process;\n"	  
+    in let print_process prev (env,s) =  
+	 let ss = delim_sprt ", " (uniq env.sens_list)
+	 in prev ^ "\n\tprocess (" ^ ss ^ ")\n\tbegin\n" ^ s ^ "\n\tend process;\n"	  
    
     in let body cobj = 
-	let stmt_attr = List.map (translate_stmt ([],"")) (cobj.fbod : Sast.s_stmt list)
-	in  List.fold_left print_process "" stmt_attr
+    let empty_env = {sens_list=[];} 
+	in let stmt_attr = List.map (translate_stmt (empty_env,"")) (cobj.fbod : Sast.s_stmt list)
+	in let s = List.fold_left print_process "" stmt_attr
+	in s
 
    in let arch cname cobj = (*arch *)
 (* need to evaluate the body before printing out the locals, because pos implies new signals! *)
-     let behavior = body cobj in
+    let behavior = body cobj 
 (* need to print out the locals nefore begin *)
-      "architecture e_" ^ cname ^ " of  " ^ cname ^ " is \n\nbegin\n" 
+
+    (* print out component list *) 
+    in let comp_decl s fdecl = 
+	    let s1 = port_gen fdecl.fid fdecl     
+	     in s ^ "component " ^ fdecl.fid ^  "\nport (\n" ^
+		"\tclk : in std_logic;\n\trst : in std_logic;\n" ^ s1 ^ ");\nend component;\n\n"  
+    in let cl_s = List.fold_left comp_decl "" cobj.fcalls    
+    in "architecture e_" ^ cname ^ " of  " ^ cname ^ " is \n\n" ^ cl_s ^ "\n\nbegin\n"
       ^ behavior
       ^ "\n\nend e_" ^ cname ^ ";\n\n"
 
@@ -141,8 +193,8 @@ let create_component cname cobj components =
 let print_programs filename components =
   (* need to iterate over all components, not just main, 
      maybe components should be a list *)
-  let s = CompMap.find "main" components 
-  (* Not essential right now: need to take in a file "adder.ehdl" and spit out "adder.vhdl"*)
+  (* let s = CompMap.find "main" components *) 
+  let s = CompMap.fold ( fun k d s -> s ^ d ) components ""   
   (*in let out_channel = open_out (filename ^ ".vhdl" ) *) 
     in let out_channel = open_out "main.vhdl"
      in output_string out_channel s  

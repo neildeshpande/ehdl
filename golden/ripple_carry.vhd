15bc53673672d1c06d635e7d40d9ff31b5f70814
paolo.mantovani86 <paolo.mantovani86@a9ed8438-15c7-ac90-eaa9-0487342f3641>
2011-12-22 21:17:32 +0000

    asd

--- golden/ripple_carry.vhd
+++ golden/ripple_carry.vhd
@@ -88,22 +88,22 @@ end component;
 
 
 type sum_type is array (0 to 3) of std_logic_vector(0 downto 0);
-signal sum_r0, sum_r4, sum_r3, sum_r2, sum_r1 : sum_type := (others => ieee.std_logic_arith.conv_std_logic_vector(0,1));
+signal sum_r0, sum_r2, sum_r1 : sum_type := (others => ieee.std_logic_arith.conv_std_logic_vector(0,1));
 type carry_type is array (0 to 3) of std_logic_vector(0 downto 0);
-signal carry_r0, carry_r4, carry_r3, carry_r2, carry_r1 : carry_type := (others => ieee.std_logic_arith.conv_std_logic_vector(0,1));
-signal overflow_r0, overflow_r4, overflow_r3, overflow_r2, overflow_r1 : std_logic_vector(0 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,1);
-signal s_r0, s_r4, s_r3, s_r2, s_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
-signal carryin_r0, carryin_r4, carryin_r3, carryin_r2, carryin_r1 : std_logic_vector(0 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,1);
-signal a_r0, a_r4, a_r3, a_r2, a_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
-signal b_r0, b_r4, b_r3, b_r2, b_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
+signal carry_r0, carry_r2, carry_r1 : carry_type := (others => ieee.std_logic_arith.conv_std_logic_vector(0,1));
+signal overflow_r0, overflow_r2, overflow_r1 : std_logic_vector(0 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,1);
+signal s_r0, s_r2, s_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
+signal carryin_r0, carryin_r2, carryin_r1 : std_logic_vector(0 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,1);
+signal a_r0, a_r2, a_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
+signal b_r0, b_r2, b_r1 : std_logic_vector(3 downto 0) := ieee.std_logic_arith.conv_std_logic_vector(0,4);
 
 
 begin
 b_r0 <= b;
 a_r0 <= a;
 carryin_r0 <= carryin;
-s <= s_r4;
-overflow <= overflow_r4;
+s <= s_r2;
+overflow <= overflow_r2;
 
 fulladder_carry0 : fulladder port map (
 		clk => clk,
@@ -114,18 +114,31 @@ fulladder_carry0 : fulladder port map (
 		sum => sum_r0(0),
 		carry => carry_r0(0));
 
+fulladder_carry1 : fulladder port map (
+		clk => clk,
+ 		rst => rst,
+		b => b_r0(1 downto 1),
+		a => a_r0(1 downto 1),
+		carryin => carry_r0(0),
+		sum => sum_r0(1),
+		carry => carry_r0(1));
+
 --Pos--
 process(clk,rst)
 begin
 if rst = '0' then
+sum_r1(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 sum_r1(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
+carry_r1(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 carry_r1(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 carryin_r1 <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 b_r1 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
 a_r1 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
 elsif clk'event and clk = '1' then
 if 1 /= 0  then
+sum_r1(1) <= sum_r0(1);
 sum_r1(0) <= sum_r0(0);
+carry_r1(1) <= carry_r0(1);
 carry_r1(0) <= carry_r0(0);
 carryin_r1 <= carryin_r0;
 b_r1 <= b_r0;
@@ -134,21 +147,34 @@ end if;
 end if;
 end process;
 
-fulladder_carry1 : fulladder port map (
+fulladder_carry2 : fulladder port map (
 		clk => clk,
  		rst => rst,
-		b => b_r1(1 downto 1),
-		a => a_r1(1 downto 1),
-		carryin => carry_r1(0),
-		sum => sum_r1(1),
-		carry => carry_r1(1));
+		b => b_r1(2 downto 2),
+		a => a_r1(2 downto 2),
+		carryin => carry_r1(1),
+		sum => sum_r1(2),
+		carry => carry_r1(2));
+
+fulladder_carry3 : fulladder port map (
+		clk => clk,
+ 		rst => rst,
+		b => b_r1(3 downto 3),
+		a => a_r1(3 downto 3),
+		carryin => carry_r1(2),
+		sum => sum_r1(3),
+		carry => carry_r1(3));
 
 --Pos--
 process(clk,rst)
 begin
 if rst = '0' then
+sum_r2(3) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
+sum_r2(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 sum_r2(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 sum_r2(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
+carry_r2(3) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
+carry_r2(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 carry_r2(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 carry_r2(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
 carryin_r2 <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
@@ -156,8 +182,12 @@ b_r2 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
 a_r2 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
 elsif clk'event and clk = '1' then
 if 1 /= 0  then
+sum_r2(3) <= sum_r1(3);
+sum_r2(2) <= sum_r1(2);
 sum_r2(1) <= sum_r1(1);
 sum_r2(0) <= sum_r1(0);
+carry_r2(3) <= carry_r1(3);
+carry_r2(2) <= carry_r1(2);
 carry_r2(1) <= carry_r1(1);
 carry_r2(0) <= carry_r1(0);
 carryin_r2 <= carryin_r1;
@@ -167,116 +197,48 @@ end if;
 end if;
 end process;
 
-fulladder_carry2 : fulladder port map (
-		clk => clk,
- 		rst => rst,
-		b => b_r2(2 downto 2),
-		a => a_r2(2 downto 2),
-		carryin => carry_r2(1),
-		sum => sum_r2(2),
-		carry => carry_r2(2));
 
---Pos--
-process(clk,rst)
-begin
-if rst = '0' then
-sum_r3(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-sum_r3(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-sum_r3(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r3(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r3(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r3(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carryin_r3 <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-b_r3 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
-a_r3 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
-elsif clk'event and clk = '1' then
-if 1 /= 0  then
-sum_r3(2) <= sum_r2(2);
-sum_r3(1) <= sum_r2(1);
-sum_r3(0) <= sum_r2(0);
-carry_r3(2) <= carry_r2(2);
-carry_r3(1) <= carry_r2(1);
-carry_r3(0) <= carry_r2(0);
-carryin_r3 <= carryin_r2;
-b_r3 <= b_r2;
-a_r3 <= a_r2;
-end if;
-end if;
-end process;
-
-fulladder_carry3 : fulladder port map (
-		clk => clk,
- 		rst => rst,
-		b => b_r3(3 downto 3),
-		a => a_r3(3 downto 3),
-		carryin => carry_r3(2),
-		sum => sum_r3(3),
-		carry => carry_r3(3));
-
---Pos--
-process(clk,rst)
-begin
-if rst = '0' then
-sum_r4(3) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-sum_r4(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-sum_r4(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-sum_r4(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r4(3) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r4(2) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r4(1) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carry_r4(0) <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-carryin_r4 <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
-b_r4 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
-a_r4 <= ieee.std_logic_arith.conv_std_logic_vector(0,4);
-elsif clk'event and clk = '1' then
-if 1 /= 0  then
-sum_r4(3) <= sum_r3(3);
-sum_r4(2) <= sum_r3(2);
-sum_r4(1) <= sum_r3(1);
-sum_r4(0) <= sum_r3(0);
-carry_r4(3) <= carry_r3(3);
-carry_r4(2) <= carry_r3(2);
-carry_r4(1) <= carry_r3(1);
-carry_r4(0) <= carry_r3(0);
-carryin_r4 <= carryin_r3;
-b_r4 <= b_r3;
-a_r4 <= a_r3;
-end if;
-end if;
-end process;
-
-
-	process (sum_r4)
+	process (sum_r2)
 	begin
-		s_r4(3 downto 3) <= sum_r4(3);
+		s_r2(3 downto 3) <= sum_r2(3);
 
 	end process;
 
 
-	process (sum_r4)
+	process (sum_r2)
 	begin
-		s_r4(2 downto 2) <= sum_r4(2);
+		s_r2(2 downto 2) <= sum_r2(2);
 
 	end process;
 
 
-	process (sum_r4)
+	process (sum_r2)
 	begin
-		s_r4(1 downto 1) <= sum_r4(1);
+		s_r2(1 downto 1) <= sum_r2(1);
 
 	end process;
 
 
-	process (sum_r4)
+	process (sum_r2)
 	begin
-		s_r4(0 downto 0) <= sum_r4(0);
+		s_r2(0 downto 0) <= sum_r2(0);
 
 	end process;
 
 
-	process (carry_r4)
+	process (a_r2, b_r2, sum_r2)
 	begin
-		overflow_r4 <= carry_r4(3);
+		if (((((((a_r2) > (0))) and (((b_r2) > (0))))) and (((sum_r2(3)) < (0))))) then 
+		overflow_r2 <= ieee.std_logic_arith.conv_std_logic_vector(1,1);
+
+		else
+		if (((((((a_r2) < (0))) and (((b_r2) < (0))))) and (((sum_r2(3)) > (0))))) then 
+		overflow_r2 <= ieee.std_logic_arith.conv_std_logic_vector(1,1);
+
+		else
+		overflow_r2 <= ieee.std_logic_arith.conv_std_logic_vector(0,1);
+		end if;
+		end if;
 
 	end process;
 
